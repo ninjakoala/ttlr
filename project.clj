a8c74120b29b153e310ab8b228cc1cc2282d24fd
Neil Prosser <neil.prosser@gmail.com>
2014-11-05 15:08:19 +0000

    Add URL to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject com.ninjakoala/ttlr "1.0.0-SNAPSHOT"
   :description "TTLR - TTL cache with refresh"
+  :url "https://github.com/ninjakoala/ttlr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
