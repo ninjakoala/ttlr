b9dc850559a788b9d844cf585ab9c6a3aae4b0c7
Neil Prosser <neil.prosser@gmail.com>
2016-05-20 22:27:42 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [overtone/at-at "1.2.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]}}
   :plugins [[lein-release "1.0.5"]]
   :lein-release {:deploy-via :clojars})
