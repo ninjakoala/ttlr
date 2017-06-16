c320bb27bd42f89c4259d71469811475b35bf441
Neil Prosser <neil.prosser@gmail.com>
2017-06-16 21:21:32 +0100

    Update project configuration

--- project.clj
+++ project.clj
@@ -3,10 +3,17 @@
   :url "https://github.com/ninjakoala/ttlr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [overtone/at-at "1.2.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2"]]}}
-  :plugins [[lein-release "1.0.5"]]
-  :lein-release {:deploy-via :clojars})
+                   :plugins [[lein-midje "3.2.1"]]}}
+  :aliases {"test" ["midje"]}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
